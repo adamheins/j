ed886af13968da7755fe6d001e3bbab8a95e6dd7
Adam Heins <mail@adamheins.com>
2020-04-28 20:48:27 -0400

    Begin setting up zunit tests.

--- j.zsh
+++ j.zsh
@@ -1,59 +1,197 @@
+# j - jump to files
 
-# Get script's directory.
-J_SRC_DIR=${0:a:h}
+[[ -z "$J_DATA_DIR" ]] && J_DATA_DIR=~/.j/data
+[[ -z "$J_IGNORE_FILE" ]] && J_IGNORE_FILE=~/.j/ignore
 
-# If the script is a symlink, we want to follow the symlink back to we can
-# access the executable.
-if [ -h $0 ]; then
-  J_SRC_DIR=$(dirname $(readlink $0))
+# if the selector isn't set, check if it is on the path
+if [[ -z "$J_SELECTOR" ]]; then
+  if command -v jselector > /dev/null 2>&1; then
+    J_SELECTOR=jselector
+  fi
 fi
 
-J_EXE=$J_SRC_DIR/j.py
 
+# Main j function.
 j() {
-  if [ -z $1 ]; then
+  if [ -z "$1" ]; then
     cd
     return
   fi
-  case $1 in
+
+  case "$1" in
     -l|--list)
-      $J_EXE --list $2
+      if [ -z "$2" ]; then
+        echo A directory name is required.
+        return 1
+      fi
+      j::list_one "$2"
     ;;
-    -p|--purge)
-      $J_EXE --purge $2
+    -p|--prune)
+      if [ -z "$2" ]; then
+        echo A directory name is required.
+        return 1
+      fi
+
+      # Check if selector is found.
+      if [ -z "$J_SELECTOR" ]; then
+        echo "Selector not found."
+      fi
+
+      if [ -f "$J_DATA_DIR/$2" ]; then
+        "$J_SELECTOR" --prune "$J_DATA_DIR/$2"
+
+        # if the file is now empty, remove the key
+        [ -s "$J_DATA_DIR/$2" ] || rm "$J_DATA_DIR/$2"
+      else
+        echo "No such key: $2"
+      fi
+    ;;
+    -c|--clean)
+      local keys key
+      IFS=$'\n'
+      keys=($(j::list_all))
+
+      for key in $keys; do
+        j::clean_one "$key" "$2"
+      done
     ;;
     -h|--help)
       echo 'j [options] dir'
       echo ''
       echo 'options:'
+      echo '  -c, --clean  remove all directories that no longer exist'
       echo '  -h, --help   show this help message and exit'
       echo '  -l, --list   list all entries for dir'
-      echo '  -p, --purge  interactively delete entries for dir'
+      echo '  -p, --prune  interactively delete entries for dir'
     ;;
     *)
-      # First allow the user to select the desired path, then cd to it.
-      $J_EXE --select $1 && cd $($J_EXE --print $1)
-      # local path=$($J_EXE --select $1)
-      # if $?; then
-      #   cd $path
-      # fi
+      if ! [ -f "$J_DATA_DIR/$1" ]; then
+        return 1
+      fi
+
+      j::clean_one "$1"
+
+      # 1. do selection
+      local d=($(j::list_one "$1"))
+      if [[ -n "${d[2]}" && -n "$J_SELECTOR" ]]; then
+        "$J_SELECTOR" "$J_DATA_DIR/$1"
+      fi
+
+      # 2. do regular change
+      d=$(j::list_one "$1" | tail -n 1)
+      if [ -n "$d" ]; then
+        cd "$d"
+      fi
     ;;
   esac
 }
 
-# Prints all keys. Used by the completion system.
-_j_print() {
-  $J_EXE --list-all-keys
+
+# Remove directories that no longer exist from a single key.
+j::clean_one() {
+  # if the key does not exist, do nothing
+  local j_path="$J_DATA_DIR/$1"
+  [ -f "$j_path" ] || return
+
+  IFS=$'\n'
+  local lines=($(<"$j_path"))
+  local tmp_file=$(mktemp)
+
+  local now min_stamp
+  min_stamp=0
+  if [ -n "$2" ]; then
+    now=$(date +%s)
+    min_stamp=$(( $now - $2*86400 ))
+  fi
+
+  # Gather lines which point to still-existing directories.
+  for line in $lines; do
+    local stamp=${line%% *}
+    if [ "$stamp" -le "$min_stamp" ]; then
+      continue
+    fi
+
+    # directory is all parts of the line after the timestamp
+    local d=${line#* }
+    if [ -d "$d" ]; then
+      echo "$line" >> "$tmp_file"
+    fi
+  done
+
+  # If the file is non-empty, replace the current file with the temp file.
+  # Otherwise, remove both.
+  if [ -s "$tmp_file" ]; then
+    mv "$tmp_file" "$j_path"
+  else
+    rm "$j_path"
+    rm "$tmp_file"
+  fi
 }
 
-_j_precmd() {
-  # Abort if the CWD doesn't exist (i.e. it was removed by some other process).
-  # This avoids error spam in the terminal.
-  [ -d $PWD ] || return
-  $J_EXE  --add-cwd
+
+# List paths associated with a single key.
+j::list_one() {
+  if [ -f "$J_DATA_DIR/$1" ]; then
+    cut -d' ' -f2- < "$J_DATA_DIR/$1"
+  fi
 }
 
-# Add to list of precmd functions.
-[[ -n "${precmd_functions[(r)_j_precmd]}" ]] || {
-  precmd_functions[$(($#precmd_functions+1))]=_j_precmd
+
+# List all keys.
+j::list_all() {
+  ls "$J_DATA_DIR"
+}
+
+
+# Exit status 0 if path should be ignored, otherwise non-zero status.
+j::is_ignored() {
+  local patterns=($(<$J_IGNORE_FILE))
+  for pattern in $patterns; do
+    [[ "$1" == $~pattern ]] && return 0
+  done
+  return 1
+}
+
+
+# Add a directory to the j database.
+# Globals:
+#   J_IGNORE_FILE
+#   J_DATA_DIR
+# Arguments:
+#   $1 Path to directory
+# Outputs:
+#   Writes time and path to a file in the j data directory.
+j::add_directory() {
+  # exit if directory doesn't exist
+  [ -d "$1" ] || return
+
+  # if the path is ignored, exit
+  if [ -f "$J_IGNORE_FILE" ]; then
+    j::is_ignored "$1" && return
+  fi
+
+  local j_path tmp_file
+  j_path="$J_DATA_DIR/$(basename $1)"
+
+  # remove existing entry of this path
+  if [ -f "$j_path" ]; then
+    tmp_file=$(mktemp)
+    grep -v "$1$" "$j_path" > "$tmp_file"
+    mv "$tmp_file" "$j_path"
+  fi
+
+  # append time and path to the file
+  echo "$(date +%s) ${1}" >> "$j_path"
+}
+
+
+# Add current working directory to the j database.
+j::add_cwd() {
+  j::add_directory "$PWD"
+}
+
+
+# Add to list of functions executed whenever working directory is changed.
+[[ -n "${chpwd_functions[(r)j::add_cwd]}" ]] || {
+  chpwd_functions[$(($#chpwd_functions+1))]=j::add_cwd
 }
